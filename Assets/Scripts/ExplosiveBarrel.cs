c3d30d33c0604af9ceb8008249b6f41970cc88be
Vincent Nguyen <vtn28@drexel.edu>
2019-01-31 00:05:22 -0500

    Add Assets

---
+++ Assets/Scripts/ExplosiveBarrel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ExplosiveBarrel : MonoBehaviour
+{
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Destroy self, spawn explosion particles
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            GameObject explosionClone = Instantiate(explosion, transform);
+            explosionClone.transform.parent = null;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LightFade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFade : MonoBehaviour
+{
+    private Light lt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lt.range -= Time.deltaTime * 7;
+    }
+}
